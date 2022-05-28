ebfccfea34791453b253adbb459417d260e6b693
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-05-28 00:24:18 -0700

    Update RGB.ino

--- tests/lights/RGB.ino
+++ tests/lights/RGB.ino
@@ -1,20 +1,32 @@
-//Check if all lights are peoperly connected or not
-int red_light_pin= 13;
-int green_light_pin = 12;
-int blue_light_pin = 11;
+int red_light_pin= 11;
+int green_light_pin = 10;
+int blue_light_pin = 9;
 void setup() {
-
-  // put your setup code here, to run once:
   pinMode(red_light_pin, OUTPUT);
   pinMode(green_light_pin, OUTPUT);
   pinMode(blue_light_pin, OUTPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-Serial.println("JSM");
-digitalWrite(red_light_pin,HIGH);
-digitalWrite(blue_light_pin,HIGH);
-digitalWrite(green_light_pin,HIGH);
-
+  RGB_color(255, 0, 0); // Red
+  delay(1000);
+  RGB_color(0, 255, 0); // Green
+  delay(1000);
+  RGB_color(0, 0, 255); // Blue
+  delay(1000);
+  RGB_color(255, 255, 125); // Raspberry
+  delay(1000);
+  RGB_color(0, 255, 255); // Cyan
+  delay(1000);
+  RGB_color(255, 0, 255); // Magenta
+  delay(1000);
+  RGB_color(255, 255, 0); // Yellow
+  delay(1000);
+  RGB_color(255, 255, 255); // White
+  delay(1000);
+}
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(red_light_pin, red_light_value);
+  analogWrite(green_light_pin, green_light_value);
+  analogWrite(blue_light_pin, blue_light_value);
 }
