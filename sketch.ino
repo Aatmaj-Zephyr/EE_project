9db113ac6d76b28a210a0b19d4ec46b1c50ec1b5
Aatmaj <Aatmaj.mhatre@gmail.com>
2022-08-04 13:12:53 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -4,14 +4,14 @@ int green_light_pin = 10;
 int blue_light_pin = 9;
 int counter=0;
 //VIBGYOR
-int red_val[]={255,0,0,255,0,255,255};
-int green_val[]={0,0,255,255,255,165,0};
-int blue_val[]={255,255,255,0,0,0,0};
+int red_val[]=   {255,0,  0,  255,0,  255,255};
+int green_val[]= {0,  0,  255,255,255,165,0};
+int blue_val[]=  {255,255,255,0,  0,  0,  0};
 int clock=0;
 int Delay=50;
 int LED_Burn_time=1000;
 int color=0;
-int levers[]={2,3,4,5,6,7,8};
+int levers[]={2,3,4,5,6,7,12};
 int score=0;
 void setup() {
   Serial.begin(9600);
@@ -24,13 +24,15 @@ void setup() {
   pinMode(5, INPUT);
   pinMode(6, INPUT);
   pinMode(7, INPUT);
-  pinMode(8, INPUT);
+  pinMode(12, INPUT);
 
 
 
 }
 void loop() {
   if(clock*Delay>=LED_Burn_time){ //next color
+      Serial.println("in loop");
+
       Serial.print(score);
       int temp=random(0,7);
       if(temp==color){ //prevent same colors comming twice
@@ -54,6 +56,8 @@ for(int i=0;i<=6;i++){
       Serial.print("-");
     }
  while(digitalRead(levers[i])==1){
+   Serial.print("pressed....");
+   Serial.print(i)
         //while button is still pressed do nothing.
         // this is to prevent multiple taps (long press)
         ;
